df13f6bc16f8817cdc778c9398de42394c082cf2
Sordhiq <39282470+Sordhiq@users.noreply.github.com>
2024-11-10 15:18:12 +0100

    Update damaturu.py

--- damaturu.py
+++ damaturu.py
@@ -14,22 +14,24 @@ def load_model():
     st.error("You have attempted to load a wrong pickle file")
     return None
 
-def pred():
+model = load_model()
 
-  rainfall_rolling = number_input(value=17.0, min_value=0.0, max_value=263.0)
-  rainfall_lag = number_input(value=50.0, min_value=0.0, max_value=400.0)
-  cumulative_rainfall = number_input(value=4500.0, min_value=1000.0, max_value=8000.0)
+def predi(rainfall_rolling, rainfall_lag, cumulative_rainfall):
+
+  rainfall_rolling = st.number_input(value=17.0, min_value=0.0, max_value=263.0)
+  rainfall_lag = st.number_input(value=50.0, min_value=0.0, max_value=400.0)
+  cumulative_rainfall = st.number_input(value=4500.0, min_value=1000.0, max_value=8000.0)
 
   features = np.array([[rainfall_rolling, rainfall_lag, cumulative_rainfall]])
   prediction = model.predict(features)
   return prediction
 
-models = pred()
+
 
 if st.button("Predict"):
-  st.success("The Predicted Malaria Cases is:", models)
+  st.success("The Predicted Malaria Cases is:", predi)
   
 st.expander("This model is proudly developed by Group 2 members of the CAN Data Science Fellowship")
 
-if __main__ == __name__:
+if __name__ == '__main__':
   main()
